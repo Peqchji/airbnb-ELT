9dcf5ee8c0079a37626198fdb31c0a08dc94cccd
Peqchji <jirapat3774@gmail.com>
2026-02-10 02:49:41 +0700

    feat: move using source

--- src/logs/query_log.sql
+++ src/logs/query_log.sql
@@ -1,19 +1,19 @@
--- created_at: 2026-02-09T19:19:38.254568300+00:00
--- finished_at: 2026-02-09T19:19:38.474448500+00:00
--- elapsed: 219ms
+-- created_at: 2026-02-09T19:47:25.770677400+00:00
+-- finished_at: 2026-02-09T19:47:25.956171900+00:00
+-- elapsed: 185ms
 -- outcome: success
 -- dialect: snowflake
 -- node_id: not available
--- query_id: 01c24da7-0000-14ba-0000-000018315e11
+-- query_id: 01c24dc3-0000-14ba-0000-00001831814d
 -- desc: Get table schema
-describe table "AIRBNB_ELT"."SOURCE"."BOOKINGS";
--- created_at: 2026-02-09T19:19:40.250038400+00:00
--- finished_at: 2026-02-09T19:19:41.737165400+00:00
--- elapsed: 1.5s
+describe table "AIRBNB_ELT"."SOURCE"."HOSTS";
+-- created_at: 2026-02-09T19:47:27.804290800+00:00
+-- finished_at: 2026-02-09T19:47:29.404816100+00:00
+-- elapsed: 1.6s
 -- outcome: success
 -- dialect: snowflake
 -- node_id: not available
 -- query_id: not available
 -- desc: dbt run query
-select * from (SELECT * FROM AIRBNB_ELT.source.bookings
+select * from (SELECT * FROM AIRBNB_ELT.source.hosts
 ) limit 1000;
